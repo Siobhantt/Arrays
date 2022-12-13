3885fe20c40c879de4e21f0e066ed9eb6739eb9b
lcardozo <lcardozo@106-25.info2>
2022-12-13 12:52:54 +0100

    oju este ejercicio

---
+++ Arrays/src/arrays/Ej9.java
@@ -0,0 +1,14 @@
+package arrays;
+
+public class Ej9 {
+/*Realiza un programa que pida la temperatura media que ha hecho en cada mes de un determinado año
+ *  y que muestre a continuación un diagrama de barras horizontales con esos datos. 
+ * Las barras del diagrama se pueden dibujar a base de asteriscos o cualquier otro carácter.*/
+	public static void main(String[] args) {
+		
+		double temperaturaMedia;
+		int año[] = new int [12];
+		
+	}
+
+}
